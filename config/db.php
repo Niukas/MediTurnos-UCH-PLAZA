c91e6468be56e715d137f626515d358ed07403d4
Niukas <plazajoaquinlg@gmail.com>
2026-05-26 22:04:40 -0300

    Clase Usuario con un metodo de login.

--- config/db.php
+++ config/db.php
@@ -6,9 +6,9 @@ $password = "654987";
 $dbname = "mediturnos";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Conexion Exitosa";
 } catch(PDOException $e) {
   echo "Conexion Fallada: " . $e->getMessage();
--- models/Usuario.php
+++ models/Usuario.php
@@ -1,5 +1,46 @@
 <?php
 
+// Clase Usuario
 
+class Usuario
+{
+    // db contiene pdo para todos los metodos de la clase
+    private $db;
 
-?>
+    public function __construct($pdo)
+    {
+        $this->db = $pdo;
+    }
+
+    // Metodo para verificar un login y devuelve un array con id, nombre, email y rol
+    public function login($email, $password)
+    {
+        try {
+
+            $sql  = "SELECT u.id_usuario,
+            u.password,
+            u.nombre,
+            u.email,
+            r.nombre AS rol
+            FROM   Usuario u
+            JOIN   Rol r ON u.id_rol = r.id_rol
+            WHERE  u.email = :email
+            LIMIT  1";
+
+            $stmt = $this->db->prepare($sql);
+            $stmt->execute([':email' => $email]);
+            $usuario = $stmt->fetch();
+
+            if ($usuario && password_verify($password, $usuario['password'])) {
+                return $usuario;
+            }else {
+                return false;
+            }
+
+        } catch (\Throwable $th) {
+            error_log($th->getMessage());
+            return false;
+        }
+    }
+
+}
